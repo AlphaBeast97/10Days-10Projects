c784c3084c2c5fe589b468bd83787ab0efb3414f
AlphaBeast97 <ksaad5272@gmail.com>
2025-05-04 17:02:00 +0500

    Day 3 some thing

---
+++ day-3/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import LandingPage from './Components/Landing/LandingPage'
+import GamePage from './Components/Content/GamePage'
+
+const App = () => {
+
+  
+
+  return (
+    <div>
+      <LandingPage />
+      <GamePage />
+    </div>
+  )
+}
+
+export default App
---
+++ day-3/src/Components/Content/GamePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const GamePage = () => {
+  return (
+    <div>
+        <nav>
+            <div className="">
+                
+            </div>
+            <div>
+                
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default GamePage
